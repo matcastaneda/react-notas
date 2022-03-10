23741da52ae69a2cc8171dcf6e56ed3259e569a8
matcastaneda <matiascmondaca@gmail.com>
2022-03-10 10:09:51 -0300

    🔥 Eliminación de `Google Analytics`

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,7 @@
 import Notes from '../components/note/Notes';
 import Footer from '../components/layout/Footer';
-import { useEffect } from 'react';
-import { googleAnalyticsAction } from '../utils/googleAnalyticsInit';
 
 const Home = () => {
-  useEffect(() => {
-    googleAnalyticsAction.initGoogleAnalytics('UA-199392592-4');
-  }, []);
-
   return (
     <>
       <Notes />
